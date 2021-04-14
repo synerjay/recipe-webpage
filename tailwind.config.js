a4eb2b8584fee611e994be2ecc1b75455964c9be
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-14 19:46:28 +0900

    bugfixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 };
+
+// Every time you change the theme in the config file, you need to run the script in the package.json
