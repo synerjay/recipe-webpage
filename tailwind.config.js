de32787f1e064b2512115596f2309f23b050455c
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-15 20:54:39 +0900

    added flex box

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
           200: '#888883',
         },
       },
+      fontFamily: {
+        body: ['Nunito'],
+      },
     },
   },
   variants: {
@@ -21,3 +24,8 @@ module.exports = {
 // Every time you change the theme in the config file, you need to run the script in the package.json
 
 // script: npm run build-css
+
+// to start the server type: live-server public
+
+// to add a custom font, import the font in the source CSS and then add the font in the extnesion in the tailwind config
+// you can name it anything... in this case you can use in html class "font-body"
