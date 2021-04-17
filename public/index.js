8906ee684eebf522e4743581e0cf47c14d85a7bc
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-17 16:26:13 +0900

    Finished tailwind practice. Needs more practice

--- public/index.js
+++ public/index.js
@@ -2,9 +2,11 @@ const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 
 burger.addEventListener('click', () => {
-  if (menu.classList.contains('hidden')) {
-    menu.classList.remove('hidden');
-  } else {
-    menu.classList.add('hidden');
-  }
+  menu.classList.toggle('hidden');
+
+  // if (menu.classList.contains('hidden')) {
+  //   menu.classList.remove('hidden');
+  // } else {
+  //   menu.classList.add('hidden');
+  // }
 });
